8646dd2a4d5553e54b16ac0b537547e61692d87a
lirang <lirang@kanzhun.com>
2019-06-18 10:35:13 +0800

    1

--- test.py
+++ test.py
@@ -3,36 +3,4 @@
 # @File    : test.py
 
 
-print 'test'
-print 'test'
-print 'test'
-print 'test3'
-print 'test4'
-print 'test'
-print 'test2'
-print 'test3'
-print 'test3'
-print 'test4'
-print 'test4'
-print 'test5'
-print 'test6'
-print 'test0'
-print 'test10'
-print 'tt'
-print 'tt'
-print 'tt'
-print 'tt'
-print 'tte'
-print 'tte'
-print 2
-print 'dev2'
-
-print 'dev3'
-
-print 'new1'
-print 'dev'
-print 'new2'
-
-print 'm1'
-print 'm2'
-print 'm3'
+print '1'
